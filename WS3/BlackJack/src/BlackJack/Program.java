1c4b84e2526f3382a2aabffb3a38c2f473375353
Olga Christensen <oc222ba@student.lnu.se>
2016-10-29 13:11:27 +0200

    Implement the operation Game::Stand

--- WS3/BlackJack/src/BlackJack/Program.java
+++ WS3/BlackJack/src/BlackJack/Program.java
@@ -4,16 +4,14 @@ import BlackJack.controller.*;
 import BlackJack.model.Game;
 import BlackJack.view.*;
 
-public class Program
-{
+public class Program {
 
-  public static void main(String[] a_args)
-  {
-  
-    Game g = new Game();
-    IView v = new SimpleView(); //new SwedishView();
-    PlayGame ctrl = new PlayGame();
-    
-    while (ctrl.Play(g, v));
-  }
+    public static void main(String[] a_args) {
+
+        Game g = new Game();
+        IView v = new SimpleView(); //new SwedishView();
+        PlayGame ctrl = new PlayGame();
+
+        while (ctrl.Play(g, v));
+    }
 }
--- WS3/BlackJack/src/BlackJack/model/rules/RulesFactory.java
+++ WS3/BlackJack/src/BlackJack/model/rules/RulesFactory.java
@@ -2,11 +2,11 @@ package BlackJack.model.rules;
 
 public class RulesFactory {
 
-  public IHitStrategy GetHitRule() {
-    return new BasicHitStrategy();
-  }
+    public IHitStrategy GetHitRule() {
+        return new BasicHitStrategy();
+    }
 
-  public INewGameStrategy GetNewGameRule() {
-    return new AmericanNewGameStrategy();
-  }
+    public INewGameStrategy GetNewGameRule() {
+        return new AmericanNewGameStrategy();
+    }
 }
